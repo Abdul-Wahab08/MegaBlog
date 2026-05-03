684fa1eb497a37e7e7bb9327e93f582a07d913dc
Abdul-Wahab08 <abdulwahab3929706@gmail.com>
2026-05-03 11:19:45 +0500

    remove unnecessary variables

--- MegaBlog/src/conf/conf.js
+++ MegaBlog/src/conf/conf.js
@@ -1,10 +1,4 @@
 const config = {
-    appWriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-    appWriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appWriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appWriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appWriteLikesId: String(import.meta.env.VITE_APPWRITE_LIKES_ID),
-    appWriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
     supabaseUrl: String(import.meta.env.VITE_SUPABASE_URL),
     supabaseKey: String(import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY)
 }
